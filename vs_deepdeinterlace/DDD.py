6962ae0a6a02f4aa0028828df8a937aee8784d77
tepete <pifroggimc@gmail.com>
2024-06-19 03:01:55 +0200

    add fp16 to DDD to improve performance

--- vs_deepdeinterlace/DDD.py
+++ vs_deepdeinterlace/DDD.py
@@ -2,7 +2,7 @@
 # Original paper by Michael Bernasconi, Abdelaziz Djelouah, Sally Hattori, and Christopher Schroers https://studios.disneyresearch.com/2020/11/10/deep-deinterlacing/
 # Unofficial implementation code, which was used for this and to train the model by vincentvdschaft https://github.com/vincentvdschaft/Disney-Deep-Deinterlacing
 
-# Vapoursynth implementation by pifroggi
+# Vapoursynth implementation and model training by pifroggi
 # or tepete on the "Enhance Everything!" Discord Server
 
 import vapoursynth as vs
@@ -16,23 +16,18 @@ from .DDD_files import DDD_arch
 core = vs.core
 
 def frame_to_array(frame: vs.VideoFrame) -> np.ndarray:
-    full_image = np.dstack([np.asarray(frame[p]) for p in range(frame.format.num_planes)])
+    array = np.empty((frame.height, frame.width, 3), dtype=np.float32)
+    for p in range(frame.format.num_planes):
+        array[..., p] = np.asarray(frame[p], dtype=np.float32)
     #remove the added border
-    half_width = full_image.shape[1] // 2
-    return full_image[:, :half_width]
+    half_width = array.shape[1] // 2
+    return array[:, :half_width]
 
 def array_to_frame(img: np.ndarray, frame: vs.VideoFrame):
     for p in range(frame.format.num_planes):
         pls = frame[p]
         frame_arr = np.asarray(pls)
         np.copyto(frame_arr, img[:, :, p])
-
-def tensor_to_array(tensor, min_max=(0, 1)):
-    tensor = tensor.squeeze().float().cpu().clamp_(*min_max)
-    tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])
-    img_np = tensor.detach().cpu().numpy()
-    img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))
-    return img_np.astype(np.float32)
 
 def apply_augmentations(arrays, inverse=False, reverse=False):
     augmented_arrays = arrays.copy()
@@ -56,8 +51,11 @@ def mirror_index(n: int, i: int, max_frames: int) -> int:
         index = 2 * (max_frames - 1) - index
     return index
 
-def inference(arrays, model, device):
+def inference(arrays, model, device, fp16=False):
     imgs_tensor = torch.from_numpy(np.ascontiguousarray(np.transpose(np.stack(arrays, axis=0), (0, 3, 1, 2)))).float().to(device)
+    if fp16:
+        imgs_tensor = imgs_tensor.half()
+
     with torch.no_grad():
         known_fields = torch.cat([imgs_tensor[0], imgs_tensor[1], imgs_tensor[2]], dim=0).to(device)
         output = model.forward(known_fields.unsqueeze(0)).detach().cpu()
@@ -65,7 +63,7 @@ def inference(arrays, model, device):
         im_network = model.sample_to_im((known_fields.unsqueeze(0).cpu(), output), num_color_channels=3)
         return im_network
 
-def process_frame(n: int, f: vs.VideoFrame, clip: vs.VideoNode, model, device, tff=False, taa=False) -> vs.VideoFrame:
+def process_frame(n: int, f: vs.VideoFrame, clip: vs.VideoNode, model, device, tff=False, tta=False, fp16=False) -> vs.VideoFrame:
     frames = [clip.get_frame(mirror_index(n, i, clip.num_frames)) for i in range(-1, 2)]
     arrays = [frame_to_array(frame) for frame in frames]
     
@@ -73,7 +71,7 @@ def process_frame(n: int, f: vs.VideoFrame, clip: vs.VideoNode, model, device, t
     if flip:
         arrays = [np.fliplr(arr) for arr in arrays]
 
-    if taa:
+    if tta:
         augmentations = [
             {'inverse': False, 'reverse': False},
             {'inverse': True, 'reverse': False},
@@ -84,13 +82,13 @@ def process_frame(n: int, f: vs.VideoFrame, clip: vs.VideoNode, model, device, t
         augmented_results = []
         for aug in augmentations:
             augmented_arrays = apply_augmentations(arrays, **aug)
-            output_img = inference(augmented_arrays, model, device)
+            output_img = inference(augmented_arrays, model, device, fp16)
             reversed_img = reverse_augmentations(output_img, **aug)
             augmented_results.append(reversed_img)
         
         output_img = np.mean(augmented_results, axis=0)
     else:
-        output_img = inference(arrays, model, device)
+        output_img = inference(arrays, model, device, fp16)
     
     if flip:
         output_img = np.fliplr(output_img)
@@ -99,7 +97,7 @@ def process_frame(n: int, f: vs.VideoFrame, clip: vs.VideoNode, model, device, t
     array_to_frame(output_img, output_frame)
     return output_frame
 
-def DDD(clip: vs.VideoNode, tff=False, taa=False, device='cuda') -> vs.VideoNode:
+def DDD(clip: vs.VideoNode, tff=False, tta=False, device='cuda', fp16=True) -> vs.VideoNode:
 
     #checks
     if clip.format.id not in [vs.RGBS]:
@@ -113,11 +111,14 @@ def DDD(clip: vs.VideoNode, tff=False, taa=False, device='cuda') -> vs.VideoNode
     config_object.config_path = str(config)
     model_class = _MODEL_FROM_TAG[config_object.model_class.lower()]
     model = model_class(config_object, device=device)
+    model.eval()
+    if fp16:
+        model.half()
 
     clip = core.std.SeparateFields(clip, tff=tff)
     #transpose because model was trained on vertical interlacing, then double frame width because for ModifyFrame input and output frames must have same dimensions
     clip = core.std.Transpose(clip)
     clip = core.std.AddBorders(clip, right=clip.width)
-    clip = clip.std.ModifyFrame(clips=[clip], selector=functools.partial(process_frame, clip=clip, model=model, device=device, tff=tff, taa=taa))
+    clip = clip.std.ModifyFrame(clips=[clip], selector=functools.partial(process_frame, clip=clip, model=model, device=device, tff=tff, tta=tta, fp16=fp16))
     clip = core.std.Transpose(clip)
     return clip
